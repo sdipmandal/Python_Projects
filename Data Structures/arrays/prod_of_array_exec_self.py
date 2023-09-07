beca39afad8697260200c85b272e2059816ce04d
sdipmandal <sdip96@gmail.com>
2023-09-07 23:31:48 +0530

    push the changes

--- Data Structures/arrays/prod_of_array_exec_self.py
+++ Data Structures/arrays/prod_of_array_exec_self.py
@@ -27,8 +27,10 @@ class Solution:
         for i in range(1, len(nums)):
             res[i] = res[i-1] * nums[i-1]
         postfix = 1
+        
         for i in range(len(nums) - 1, -1, -1):
             res[i] *= postfix
             postfix *= nums[i]
+        
         return res
             
