13e7c9cf13424a8ef10e22923d0115edfeaf0c02
sdipmandal <sdip96@gmail.com>
2023-10-16 00:37:12 +0530

    displaying values instead of object in admin

--- office_employee/office_emp/emp_app/models.py
+++ office_employee/office_emp/emp_app/models.py
@@ -5,8 +5,14 @@ class Department(models.Model):
     name = models.CharField(max_length=100, null=False)
     location = models.CharField(max_length=100)
 
+    def __str__(self) -> str:
+        return self.name
+
 class Role(models.Model):
     name = models.CharField(max_length=100, null=False)
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Employee(models.Model):
@@ -18,3 +24,6 @@ class Employee(models.Model):
     role = models.ForeignKey(Role, on_delete=models.CASCADE)
     phone = models.IntegerField(default=0)
     hire_date = models.DateField()
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
